fff4686dada96209164239cc5be6337137f5f16c
srfvidov <fkor3@AMK.cpq>
2023-05-31 11:31:34 +0800

    Initial Commit

---
+++ ,lkjhb/_lkjhbApp.swift
@@ -0,0 +1,17 @@
+//
+//  _lkjhbApp.swift
+//  ,lkjhb
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct _lkjhbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
